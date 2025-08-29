58e477dec75fdbc3786e8753ebb12b878dfb6c4b
Gav5193 <gavinge123@gmail.com>
2025-08-28 21:31:56 -0400

    switche host

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "http://172.105.104.94/", // or wherever the db is hosted
+  host: "172.105.104.94", // or wherever the db is hosted
   user: "gavin",
   database: "memtest",
   password: "Gg1096583!",
